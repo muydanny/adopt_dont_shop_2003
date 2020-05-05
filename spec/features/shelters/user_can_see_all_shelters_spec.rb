38a3919375538226198de3c717c3cd253a494e6e
Danny Ramos <danramos21@gmail.com>
2020-05-05 15:20:21 -0600

    Added Shelter Index

---
+++ spec/features/shelters/user_can_see_all_shelters_spec.rb
@@ -0,0 +1,13 @@
+require "rails_helper"
+
+RSpec.describe "shelters index page", type: :feature do
+  it "can see all names of the shelters in system" do
+    shelter_1 = Shelter.create(name: "Dumb Friends League")
+    shelter_2 = Shelter.create(name: "Pick a Pal")
+
+    visit "/shelters"
+
+    expect(page).to have_content(shelter_1.name)
+    expect(page).to have_content(shelter_2.name)
+  end
+end
