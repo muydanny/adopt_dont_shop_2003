2f8e8630f67563e41d6573755a91a7a0db14301d
Danny Ramos <danramos21@gmail.com>
2020-05-05 22:38:53 -0600

    passing test in story one

--- spec/features/shelters/user_can_see_all_shelters_spec.rb
+++ spec/features/shelters/user_can_see_all_shelters_spec.rb
@@ -1,11 +1,11 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe "shelters index page", type: :feature do
   it "can see all names of the shelters in system" do
     shelter_1 = Shelter.create(name: "Dumb Friends League")
     shelter_2 = Shelter.create(name: "Pick a Pal")
 
-    visit "/shelters"
+    visit '/shelters'
 
     expect(page).to have_content(shelter_1.name)
     expect(page).to have_content(shelter_2.name)
