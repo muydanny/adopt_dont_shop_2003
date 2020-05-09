4eaf04bf6e2b68e114c5be90bcda6dffedfa025b
Danny Ramos <danramos21@gmail.com>
2020-05-09 12:00:52 -0600

    US10

--- spec/features/shelters/pets/user_can_create_a_new_pet_spec.rb
+++ spec/features/shelters/pets/user_can_create_a_new_pet_spec.rb
@@ -2,28 +2,29 @@ require 'rails_helper'
 
 RSpec.describe "When I visit a Shelter Pets Index page" do
   it "can create a new shelter" do
-    visit '/shelters/:shelter_id/pets'
     shelter = create(:shelter)
     pet = create(:pet, shelter: shelter)
-    expect(page).to have_content("All Pets")
-    expect(page).not_to have_content("Pet name")
+
+    visit "/shelters/#{shelter.id}/pets"
+    expect(find_button("Create Pet")).to be_truthy
+    expect(page).not_to have_content("Pet Name")
 
     click_button("Create Pet")
 
-    expect(current_path).to eq '/shelters/:shelter_id/pets/new'
+    expect(current_path).to eq "/shelters/#{shelter.id}/pets/new"
     expect(page).to have_content("Create a new Pet")
 
-    fill_in('pet_image', :with => 'Pet Image')
+    fill_in('pet_image', :with => 'https://whatyouth.com/wp-content/uploads/2016/03/whatyouth_lizard_submission_photo-256x256.jpg')
     fill_in('pet_name', :with => 'Pet Name')
     fill_in('pet_description', :with => 'Pet Description')
-    fill_in('pet_age', :with => 'Pet age')
+    fill_in('pet_approximate_age', :with => 'Pet age')
     fill_in('pet_sex', :with => 'Female or Male')
 
     click_button("Create Pet")
 
-    expect(current_path).to eq '/shelters/:shelter_id/pets'
+    expect(current_path).to eq "/shelters/#{shelter.id}/pets"
 
-    expect(page).to have_content("Pet name")
+    expect(page).to have_content("Pet Name")
   end
 end
 
