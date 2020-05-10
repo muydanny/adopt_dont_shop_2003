83feea25edbcc05f9096df1b513d1265c0df6708
Danny Ramos <danramos21@gmail.com>
2020-05-09 21:18:57 -0600

    Added link to index

--- spec/features/shelters/user_can_see_all_shelters_spec.rb
+++ spec/features/shelters/user_can_see_all_shelters_spec.rb
@@ -2,6 +2,7 @@ require 'rails_helper'
 
 RSpec.describe "shelters index page", type: :feature do
   it "can see all names of the shelters in system" do
+    it "can click a link next to every shelter name to edit info"
     shelter_1 = Shelter.create(name: "Dumb Friends League")
     shelter_2 = Shelter.create(name: "Pick a Pal")
 
@@ -9,5 +10,6 @@ RSpec.describe "shelters index page", type: :feature do
 
     expect(page).to have_content(shelter_1.name)
     expect(page).to have_content(shelter_2.name)
+    expect(page.find("/shelters/#{shelter.id}/edit")).to be_truthy
   end
 end
